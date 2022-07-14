4aecab10f964ae007a83d70140641cbb29d81ed9
Harsh Soni <69109482+hashfx@users.noreply.github.com>
2022-07-14 12:34:03 +0000

    Basic Types / Numbers exercises done

---
+++ Basic Types/Numbers/2.rs
@@ -0,0 +1,17 @@
+/*
+
+//  Fill the blank
+fn main() {
+    let v: u16 = 38_u8 as __;
+
+    println!("Success!");
+}
+
+*/
+
+//  Fill the blank
+fn main() {
+    let v: u16 = 38_u8 as u16;  // added u16
+
+    println!("Success!");
+}
