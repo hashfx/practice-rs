b7882627c0e75814a974d04edf41504bfe1ea2b2
Harsh Soni <69109482+hashfx@users.noreply.github.com>
2022-07-14 13:07:17 +0000

    functions

---
+++ Basic Types/Functions/2.rs
@@ -0,0 +1,21 @@
+/*
+
+fn main() {
+   print();
+}
+
+// Replace i32 with another type
+fn print() -> i32 {
+   println!("Success!");
+}
+
+*/
+
+fn main() {
+    print();
+ }
+ 
+ fn print() -> () {  // no return type
+    println!("Success!");
+ }
+ 
