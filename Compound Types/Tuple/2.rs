4ab9d124f24b7381e2f8df975e127ea0cba8714d
Harsh Soni <69109482+hashfx@users.noreply.github.com>
2022-07-16 09:54:04 +0000

    Tuple

---
+++ Compound Types/Tuple/2.rs
@@ -0,0 +1,18 @@
+/*
+
+// Make it work
+fn main() {
+    let t = ("i", "am", "sunface");
+    assert_eq!(t.1, "sunface");
+
+    println!("Success!");
+}
+
+*/
+
+fn main() {
+    let t = ("i", "am", "sunface");
+    assert_eq!(t.2, "sunface");
+    
+    println!("Success!");
+}
