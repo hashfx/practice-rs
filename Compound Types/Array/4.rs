8209dc7b7dfdd949a6adbc36961bbb9fbe4246a6
Harsh Soni <69109482+hashfx@users.noreply.github.com>
2022-07-16 09:31:43 +0000

    Array

---
+++ Compound Types/Array/4.rs
@@ -0,0 +1,17 @@
+/*
+
+fn main() {
+    // Fix the error
+    let _arr = [1, 2, '3'];
+
+    println!("Success!");
+}
+
+*/
+
+fn main() {
+    // fix the error
+    let _arr = [1, 2, 3];
+    
+    println!("Success!");
+}
