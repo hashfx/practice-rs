4eac591dbfbac39918697abb56876c7a5e09e06e
Harsh Soni <69109482+hashfx@users.noreply.github.com>
2022-07-16 09:48:28 +0000

    slice

---
+++ Compound Types/Slice/6.rs
@@ -0,0 +1,35 @@
+/*
+
+// Fix errors
+fn main() {
+    let mut s = String::from("hello world");
+
+    // Here, &s is `&String` type, but `first_word` need a `&str` type.
+    // It works because `&String` can be implicitly converted to `&str, If you want know more ,this is called `Deref` 
+    let word = first_word(&s);
+
+    s.clear(); // error!
+
+    println!("the first word is: {}", word);
+}
+fn first_word(s: &str) -> &str {
+    &s[..1]
+}
+
+*/
+
+fn main() {
+    let mut s = String::from("hello world");
+
+    // here, &s is `&String` type, but `first_word` need a `&str` type.
+    // it works because `&String` can be implicitly converted to `&str, If you want know more ,this is called `Deref` 
+    let word = first_word(&s);
+
+    println!("the first word is: {}", word);
+
+    s.clear();
+}
+
+fn first_word(s: &str) -> &str {
+    &s[..1]
+}
