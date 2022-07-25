63f5a3c53e3806c739f8ace15b3ec325d221752d
Harsh Soni <69109482+hashfx@users.noreply.github.com>
2022-07-25 11:06:41 +0000

    struct

--- Compound Types/Struct/1.rs
+++ Compound Types/Struct/1.rs
@@ -1,1 +1,34 @@
+/*
 
+// Fix the error
+struct Person {
+    name: String,
+    age: u8,
+    hobby: String
+}
+fn main() {
+    let age = 30;
+    let p = Person {
+        name: String::from("sunface"),
+        age,
+    };
+
+    println!("Success!");
+}
+
+*/
+
+struct Person {
+    name: String,
+    age: u8,
+    hobby: String,
+}
+fn main() {
+    let age = 30;
+    let p = Person {
+        name: String::from("sunface"),
+        age,
+        hobby: "coding".to_string(),
+    };
+    println!("Success!");
+}
