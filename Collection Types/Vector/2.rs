378c49fa25690539e32d59d0ef74a2e4cef2fc56
Harsh Soni <69109482+hashfx@users.noreply.github.com>
2022-07-26 12:22:30 +0000

    vector collection

---
+++ Collection Types/Vector/2.rs
@@ -0,0 +1,30 @@
+/*
+
+// FILL in the blank
+fn main() {
+    let mut v1 = Vec::from([1, 2, 4]);
+    v1.pop();
+    v1.push(3);
+
+    let mut v2 = Vec::new();
+    v2.__;
+
+    assert_eq!(v1, v2);
+
+    println!("Success!")
+}
+
+*/
+
+fn main() {
+    let mut v1 = Vec::from([1, 2, 4]);
+    v1.pop();
+    v1.push(3);
+
+    let mut v2 = Vec::new();
+    v2.extend([1, 2, 3]);
+
+    assert_eq!(format!("{:?}", v1), format!("{:?}", v2));
+
+    println!("Success!")
+}
