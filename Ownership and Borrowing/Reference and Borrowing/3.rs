4f9952424b22866c6ca71e98e5bf35ee63080fd9
Harsh Soni <69109482+hashfx@users.noreply.github.com>
2022-07-26 10:47:07 +0000

    reference and borrowing

---
+++ Ownership and Borrowing/Reference and Borrowing/3.rs
@@ -0,0 +1,24 @@
+/*
+
+// Fix error
+fn main() {
+    let mut s = String::from("hello, ");
+
+    borrow_object(s);
+
+    println!("Success!");
+}
+
+fn borrow_object(s: &String) {}
+
+*/
+
+fn main() {
+    let mut s = String::from("hello, ");
+
+    borrow_object(&s);
+
+    println!("Success!");
+}
+
+fn borrow_object(s: &String) {}
