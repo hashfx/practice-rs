465321aba2a4924771074848fa7896cddf01edf6
Harsh Soni <69109482+hashfx@users.noreply.github.com>
2022-07-26 12:18:18 +0000

    string collection

---
+++ Collection Types/String/3.rs
@@ -0,0 +1,24 @@
+/*
+
+// Question: how many heap allocations are happened here ?
+// Your answer:
+fn main() {
+    // Create a String type based on `&str`
+    // the type of string literals is `&str`
+   let s: String = String::from("hello, world!");
+
+   // create a slice point to String `s`
+   let slice: &str = &s;
+
+   // create a String type based on the recently created slice
+   let s: String = slice.to_string();
+
+   assert_eq!(s, "hello, world!");
+
+   println!("Success!")
+}
+
+*/
+fn main() {
+    println!("2. Success!");
+}
