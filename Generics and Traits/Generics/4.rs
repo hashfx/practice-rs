204275ec8e76d989df4b4ca3438f3052939a18a3
Harsh Soni <69109482+hashfx@users.noreply.github.com>
2022-07-26 12:41:28 +0000

    generics

---
+++ Generics and Traits/Generics/4.rs
@@ -0,0 +1,32 @@
+/*
+
+// Modify this struct to make the code work
+struct Point<T> {
+    x: T,
+    y: T,
+}
+
+fn main() {
+    // DON'T modify this code.
+    let p = Point{x: 5, y : "hello".to_string()};
+
+    println!("Success!");
+}
+
+*/
+
+// modify this struct to make the code work
+struct Point<T, U> {
+    x: T,
+    y: U,
+}
+
+fn main() {
+    // DON'T modify here
+    let p = Point {
+        x: 5,
+        y: "hello".to_string(),
+    };
+
+    println!("Success!");
+}
